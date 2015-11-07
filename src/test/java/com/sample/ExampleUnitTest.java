3ad738613938d8a28470337be9a84289a117e75c
xuejianqing <xuejianqing@star-net.cn>
2015-11-07 21:20:51 +0800

    add unit test

---
+++ src/test/java/com/sample/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleUnitTest {
+
+	@Test
+	public void test() {
+		int value = 5 + 5;
+		assertTrue(value == 10);
+	}
+
+}
